977b631628c1d25aa168a59b82ca042478cd78c0
ta2edchimp <kontakt@andreaswindt.de>
2016-03-17 17:21:46 +0100

    fix(tests): Correct link to required bin.js

--- test/bin.js
+++ test/bin.js
@@ -3,9 +3,7 @@ import test from 'ava'
 test.todo('test bin later')
 
 try {
-  require('./bin') // requiring now for coverage until this is tested
+  require('../bin') // requiring now for coverage until this is tested
 } catch (error) {
   // ignore the inevitable error
 }
-
-
