e2cf04e10b0820c10165ad2c654ee518c9f31ee9
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-06 14:34:24 -0700

    feat(cli): accepts options corresponding to rules
    -c|--current, -p|--plugin, -a|--all-available, -u|--unused
    
    BREAKING: updated package name

--- src/bin.js
+++ src/bin.js
@@ -2,15 +2,29 @@
 
 'use strict'
 
-// Prints rules recognized by ESLint that don't appear in the given config
-// preset. It helps with upgrading the preset when new ESLint gets released.
-var getRuleFinder = require('./rule-finder')
-var specifiedFile = process.argv[2]
-var ruleFinder = getRuleFinder(specifiedFile)
-
-var newRules = ruleFinder.getUnusedRules()
-
-if (newRules.length) {
-  console.log('New rules to add to the config: ' + newRules.join(', ') + '.') // eslint-disable-line no-console
-  process.exit(1)
+var options = {
+  getCurrentRules: ['current', 'c'],
+  getPluginRules: ['plugin', 'p'],
+  getAllAvailableRules: ['all-available', 'a'],
+  getUnusedRules: ['unused', 'u'],
 }
+
+var argv = require('yargs')
+  .boolean(Object.keys(options))
+  .alias(options)
+  .argv
+
+var getRuleFinder = require('./rule-finder')
+var specifiedFile = argv._[0]
+
+var ruleFinder = getRuleFinder(specifiedFile)
+Object.keys(options).forEach(function findRules(option) {
+  var rules
+  if (argv[option]) {
+    rules = ruleFinder[option]()
+    if (rules.length) {
+      console.log('\n' + options[option][0], 'rules\n') // eslint-disable-line no-console
+      console.log(rules.join(', ')) // eslint-disable-line no-console
+    }
+  }
+})
