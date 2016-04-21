4cc7b9846a0e5be5bac445a033cf9943a7d109a7
Andreas Windt <ta2edchimp@users.noreply.github.com>
2016-04-21 10:00:29 +0200

    refactor(output): render output via a utility wrapper (#58)

--- src/bin/diff.js
+++ src/bin/diff.js
@@ -2,9 +2,7 @@
 
 'use strict'
 
-var size = require('window-size')
-var availableWidth = size.width || /* istanbul ignore next */ 80
-var ui = require('cliui')({width: availableWidth})
+var cli = require('../lib/cli-util')
 
 var getRuleFinder = require('../lib/rule-finder')
 var difference = require('../lib/array-diff')
@@ -17,30 +15,10 @@ var rules = getSortedRules(
   )
 )
 
-var outputRules, outputRuleCellMapper
-var outputPadding = ' '
-var outputMaxWidth = 0
-var outputMaxCols = 0
-
 /* istanbul ignore next */
 if (rules.length) {
-  console.log('\n diff rules\n') // eslint-disable-line no-console
-  rules = rules.map(function columnSpecification(rule) {
-    rule = rule + outputPadding
-    outputMaxWidth = Math.max(rule.length, outputMaxWidth)
-    return rule
-  })
-  outputMaxCols = Math.floor(availableWidth / outputMaxWidth)
-  outputRuleCellMapper = getOutputRuleCellMapper(Math.floor(availableWidth / outputMaxCols))
-  while (rules.length) {
-    outputRules = rules.splice(0, outputMaxCols).map(outputRuleCellMapper)
-    ui.div.apply(ui, outputRules)
-  }
-  console.log(ui.toString()) // eslint-disable-line no-console
+  cli.push('\ndiff rules\n')
+  cli.push(rules)
 }
 
-function getOutputRuleCellMapper(width) {
-  return function curriedOutputRuleCellMapper(rule) {
-    return {text: rule, width: width}
-  }
-}
+cli.write()
---
+++ src/lib/cli-util.js
@@ -0,0 +1,41 @@
+var size = require('window-size')
+var availableWidth = size.width || /*istanbul ignore next */ 80
+var ui = require('cliui')({width: availableWidth})
+
+function push(output, columns) {
+  var _output = [].concat(output)
+
+  var padding = {top: 0, right: 2, bottom: 0, left: 0}
+  var width = _output.reduce(
+    function getMaxWidth(previous, current) {
+      return Math.max(padding.left + current.length + padding.right, previous)
+    }, 0)
+
+  var _columns = columns || Math.floor(availableWidth / width)
+  var cellMapper = getOutputCellMapper(Math.floor(availableWidth / _columns), padding)
+
+  while (_output.length) {
+    ui.div.apply(ui, _output.splice(0, _columns).map(cellMapper))
+  }
+}
+
+function write(logger) {
+  var _logger = logger || console
+  var _log = _logger.log || /* istanbul ignore next */ console.log // eslint-disable-line no-console
+  _log(ui.toString())
+}
+
+function getOutputCellMapper(width, padding) {
+  return function curriedOutputCellMapper(text) {
+    return {
+      text: text,
+      width: width,
+      padding: [padding.top, padding.right, padding.bottom, padding.left],
+    }
+  }
+}
+
+module.exports = {
+  push: push,
+  write: write,
+}
