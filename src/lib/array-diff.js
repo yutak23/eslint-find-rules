8db1029e5a67425ab87d8292ab779ec0a2b4e5ef
ta2edchimp <kontakt@andreaswindt.de>
2017-06-12 11:22:01 +0200

    chore(tests): also run tests on Node 4.x

--- src/lib/array-diff.js
+++ src/lib/array-diff.js
@@ -1,5 +1,5 @@
 function difference(a, b) {
-  return a.filter(item => !b.includes(item)).sort((a, b) => a > b);
+  return a.filter(item => b.indexOf(item) === -1).sort((a, b) => a > b);
 }
 
 module.exports = difference;
